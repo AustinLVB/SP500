6fef96ffdd7cb15e0a3780735e7b1c68bad7b7c8
Laura Bishop <lvbishop@gmail.com>
2017-03-29 14:31:47 -0500

    Not needed

--- SPplot.R
+++
@@ -1,9 +0,0 @@
-# Plot the results, overlaying the volatility curves on the data, just as was done in the S&P example.
-plot(volest,type="l")
-
-# TODO: Add connected line segments for volest2 with the parameters: type="l",col="red"
-# hint: look at oilExerciseCode.R file at the live discussion
-lines(volest2, type="l",col="red")
-
-# TODO: Add connected line segments for volest3 with the parameters: type="l",col="blue"
-lines(volest3, type="l",col="yellow")
