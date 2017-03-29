8e195396e635a068defcf4b70d239e0a5e557e05
Laura Bishop <lvbishop@gmail.com>
2017-03-29 14:26:21 -0500

    Plot make file
    Made a plotting make file that pulls SP500 file from my GitHub
    directory.

--- SPCalculate.R
+++ SPCalculate.R
@@ -22,17 +22,17 @@ cat ("is the volatility measure. \n")
 
 ## Define getVol function for volatility
 getVol <- function(d, logrets) {
-	var = 0
-	lam = 0
-	varlist <- c()
-
-	for (r in logrets) {
-		lam = lam*(1 - 1/d) + 1
-	  var = (1 - 1/lam)*var + (1/lam)*r^2
-		varlist <- c(varlist, var)
-	}
-
-	sqrt(varlist)
+  var = 0
+  lam = 0
+  varlist <- c()
+  
+  for (r in logrets) {
+    lam = lam*(1 - 1/d) + 1
+    var = (1 - 1/lam)*var + (1/lam)*r^2
+    varlist <- c(varlist, var)
+  }
+  
+  sqrt(varlist)
 }
 
 
@@ -48,14 +48,3 @@ volest2 <- getVol (30,SNPret)
 
 # TODO: call getVol function with the parameters: 100,SNPret
 volest3 <- getVol(100,SNPret)
-
-
-# Plot the results, overlaying the volatility curves on the data, just as was done in the S&P example.
-plot(volest,type="l")
-
-# TODO: Add connected line segments for volest2 with the parameters: type="l",col="red"
-# hint: look at oilExerciseCode.R file at the live discussion
-lines(volest2, type="l",col="red")
-
-# TODO: Add connected line segments for volest3 with the parameters: type="l",col="blue"
-lines(volest3, type="l",col="red")
